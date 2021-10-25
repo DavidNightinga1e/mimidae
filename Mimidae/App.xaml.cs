59a4e51ba5b28dde03d7ba233ce76604d4d34649
David Nightingale <nightrider2552@gmail.com>
2021-10-25 18:22:39 +0300

    init

---
+++ Mimidae/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mimidae
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Mimidae/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Mimidae
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
