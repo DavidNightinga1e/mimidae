84c6e3dd9df57ae91836428614447ca18c0ff3ae
David Nightingale <nightrider2552@gmail.com>
2021-10-25 18:43:59 +0300

    add and remote test

--- Mimidae/MainWindow.xaml.cs
+++ Mimidae/MainWindow.xaml.cs
@@ -1,13 +1,43 @@
-﻿namespace Mimidae
+﻿using System;
+using System.Windows;
+
+namespace Mimidae
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            TorrentsListBox.Items.Add(DateTime.Now.Ticks);
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PauseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void StopButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = TorrentsListBox.SelectedItems;
+            if (selectedItems.Count is 1)
+            {
+                var item = selectedItems[0];
+                TorrentsListBox.Items.Remove(item);
+            }
         }
     }
 }
